90828d3027fb8c3f4f3b3ce822dfe46feff8366e
Sheyla <sheyla.ramirez2110@gmail.com>
2020-07-19 22:30:01 -0500

    Se agrego el archivo js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 import validator from './validator.js';
 
 console.log(validator);
+
+
+function (GuardarDigitos) {
+        // Obtiene el valor puesto en número de tarjeta
+        var digitos = document.getElementById("PonerNumero").value;}
+    alert("HOLA");
+        // Guardo informacion en el storage
+
+    if (typeof(Storage) !== "undefined") {
+        localStorage.setItem("PonerNumero", Digitos);
+
+
+    alert(Digitos);
+
+  }
+}
