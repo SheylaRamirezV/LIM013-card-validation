1bae521b7d9657b930df89bb67baa3c23465cd53
SheylaRamirezV <68232942+SheylaRamirezV@users.noreply.github.com>
2020-08-01 13:43:24 -0500

    FONDO FUNCION IS VALID Y MASKIFY

--- src/index.js
+++ src/index.js
@@ -6,38 +6,9 @@ let btn = document.getElementById("btn");
 btn.addEventListener("click", ValidarDigitos)
 
 function ValidarDigitos() {
-        // Obtiene el valor del elemento del Id
+        // Obtiene el numero de tarjeta
        let NumeroDeTarjeta= document.getElementById("PonerNumero").value;
-       validator.isValid(NumeroDeTarjeta)
-       validator.maskify(NumeroDeTarjeta)
-
-    console.log("Numero de Tarjeta es =" + NumeroDeTarjeta);
-
-      // genero un arreglo para que mi cadena se guarde por cada digito.
-      let ArregloNumeroDeTarjeta= NumeroDeTarjeta.split("");
-    console.log(ArregloNumeroDeTarjeta);
-      //Pone mi arreglo en reverso
-      let NumeroDeTarjetaReverso= ArregloNumeroDeTarjeta.reverse();
-    console.log(NumeroDeTarjetaReverso);
-
-    //Empieza mi validación en Luhm
-   let SumaTotal= 0;
-     for(let i=0; i<NumeroDeTarjetaReverso.length; i++){
-       let CadaNumero= NumeroDeTarjetaReverso[i];
-       if((i % 2) != 0){
-         // arregloVacio.push(CadaNumero *= 2;)
-           CadaNumero *= 2;
-             if(CadaNumero > 9){
-               CadaNumero -= 9;
-           }
-       }
-       SumaTotal += CadaNumero
-   }
-       if( SumaTotal % 10 ==0)
-     {
-   return alert("LA TARJETA ES VALIDA");
-     }
- else{
-   return alert("LA TARJETA ES INVALIDA");
-     }
-}
+       //importamos el validator con la función IsValid y maskify
+       validator.isValid(NumeroDeTarjeta);
+       validator.maskify(NumeroDeTarjeta);
+  }
--- src/validator.js
+++ src/validator.js
@@ -1,11 +1,57 @@
 const validator = {
-  //...
-  isValid:function(numero){
-    console.log(numero)
-  },
-  maskify:function(num){
-    console.log(num)
+  isValid:
+  function(NumeroDeTarjeta){
+    //Genero un Arreglo para que mi numero general sea de manera unitaria
+    let ArregloNumeroDeTarjeta= NumeroDeTarjeta.split("");
+  console.log(ArregloNumeroDeTarjeta);
+    //Pone mi arreglo en reverso
+    let NumeroDeTarjetaReverso= ArregloNumeroDeTarjeta.reverse();
+  console.log(NumeroDeTarjetaReverso);
+  //Empieza mi validación en Luhm
+  //genero una variable Sumatotal = 0
+ let SumaTotal= 0;
+ // para que mi variable i pueda recorrer debe llegar hasta 1 mes de 16 (posicion 15) y mi variable sera 1 a 1
+   for(let i=0; i<NumeroDeTarjetaReverso.length; i++){
+     // genero una nueva variable CadaPar - parseInt para que el numero sea entero y se trabaja con NumeroDeTarjetaReverso
+     let CadaPar= parseInt(NumeroDeTarjetaReverso[i]);
+     // Si mi numero divido entre dos su residuo es diferente de cero (entramos a las condicionales)
+     if((i % 2) != 0){
+       // Mi nueva variable cadapar se multiplica por dos
+         CadaPar *= 2;
+         // Si es el resultado es mayor a nueve , entonces se le resta nueve.
+           if(CadaPar > 9){
+             CadaPar -= 9;
+         }
+     }
+     // mi variable Suma Total se sumara cada numero incluido las posiciones CadaPar.
+     SumaTotal += CadaPar;
+ }
+ console.log(SumaTotal);
+ // Si mi SumaTotal si al dividir entre 10 es igual a cero
+     if( SumaTotal % 10 ==0)
+   {
+ return alert("LA TARJETA ES VALIDA");
+   }
+else{
+ return alert("LA TARJETA ES INVALIDA");
   }
-};
+},
+
+
+maskify:
+function(Ocultar){
+  let maskify2= "";
+  for(let i=0; i<Ocultar.length; i++){
+    if(i<= Ocultar.length -5){
+      maskify2=maskify2 + "*"
+    } else {
+      maskify2=maskify2 + Ocultar[i]
+
+    }
+  }
+console.log(maskify2);
+return maskify2
+ }
+}
 
 export default validator;
